eeb00ec99a09d5fefd5ceb7fd12b459925910344
Owain Lanfear <owainlanfear@icloud.com>
2025-02-08 22:07:28 +0000

    inital upload

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 
 app.use("/static", express.static("./static"));
 app.get("/", (req, res) => res.type("html").send(html));
-app.get("/gim", (req, res) =>
-    res.redirect("https://www.goedkoopisolatiemateriaal.nl")
-);
-app.get("/fly", (req, res) =>
+
+app.get("/spy", (req, res) =>
     res.redirect("https://www.flyspeedy.com")
 );
-app.get("/flyops", (req, res) =>
-    res.redirect("https://www.flyspeedy.com/opscenter")
-);
-app.get("/gh", (req, res) =>
-    res.redirect("https://www.github.com/jaoord")
-);
+
 app.get("/links", (req, res) =>
-    res.redirect("https://app.raindrop.io/my/0")
+    res.redirect("https://www.google.com")
 );
 
-
+app.get("/adm", (req, res) =>
+    res.redirect("")
+);
 const server = app.listen(port, () =>
-    console.log(`yogm.dev listening on port ${port}!`)
+    console.log(`listening on port ${port}!`)
 );
 
 server.keepAliveTimeout = 120 * 1000;
@@ -34,13 +29,17 @@ const html = `
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>yogm.dev</title>
+    <title>owain.dev</title>
    <link rel="stylesheet" href="./static/main.css" type="text/css">
   </head>
   <body id="home">
     <section>
-        <h1>yogm.dev</h1>
+        <h1>owain.dev</h1>
         <p>I &#9829; code</p>
+        <p>welcome to my website :)</p>
+        <h2>Links</h2>
+        <p>/links /spy /help<p>
+        <p>hello world</p>
     </section>
   </body>
 </html>
